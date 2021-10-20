7a06c150a052c1af76921290859be4560d674f47
akyeshi <akyeshi@gmail.com>
2021-10-19 21:01:15 -0400

    added 3 files

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function method1(){
+    alert("hello world"); 
+}
+
+function clickTextAnalyzer(){
+    var value1 = document.getElementById("textAnalyzer").value; 
+    console.log(value1); 
+
+}
+
+function clickImageAnalyzer(){
+    var value2 = document.getElementById("imageAnalyzer").value; 
+    console.log(value2); 
+
+}
